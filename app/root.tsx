aef04f2f915578183f5ad6237ea81be300d95146
Alexandru Noroc <kradores10@gmail.com>
2026-05-08 00:16:37 +0200

    remove meta

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,6 @@
 import {
   isRouteErrorResponse,
   Links,
-  Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -19,11 +18,10 @@ export const links: Route.LinksFunction = () => [];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
         <Links />
       </head>
       <body>
